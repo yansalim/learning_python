3a709d547652282c7c85b9a83a29e2ea458c5501
yansalim <contato@crosoften.com>
2022-04-15 22:08:09 -0300

    11

---
+++ 11-Calculos.py
@@ -0,0 +1,30 @@
+print('Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:')
+print('- O produto do dobro do primeiro com metade do segundo .')
+print('- A soma do triplo do primeiro com o terceiro.')
+print('- O terceiro elevado ao cubo.')
+
+A = int(input('insira um numero inteiro: '))
+B = int(input('insira outro numero inteiro: '))
+C = float(input('insira um número real: '))
+
+print('a) o produto do dobro do primeiro com metade do segundo .\n')
+
+D = (2*A)*(B/2)
+print('Resposta a: ' , D)
+
+print('b) a soma do triplo do primeiro com o terceiro.')
+
+E = (3*A)+C
+
+print('Resposta b: ', E)
+
+
+print('c) o terceiro elevado ao cubo.')
+
+F = C**3
+
+print('Resposta c: ', F)
+
+
+
+
